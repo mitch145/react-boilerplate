fcd81661d54a91c046efee6452b56eb2ba733588
Mitch Ball <mitchb145@gmail.com>
2016-09-18 19:45:42 +1000

    Added basic todo structure

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
-import HelloWorld from './hello-world';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './todo-list';
 
 ReactDOM.render(
-    <HelloWorld phrase="my new test page"/>,
-    document.querySelector('.root')
+    <TodoList/>,
+    document.getElementById('app')
 );
